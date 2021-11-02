9acc587a7ab908e794e0bf4de937567b47f68dd6
Cl4pTrap <tixooful@gmail.com>
2021-11-02 11:59:23 +1000

    raid.sh

---
+++ raid.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mdadm --zero-superblock --force /dev/sd{b,c,d,e,f,g}
+y | mdadm --create --verbose /dev/md0 -l 10 -n 4 /dev/sd{b,c,d,e}
+
+mkdir /etc/mdadm
+echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
